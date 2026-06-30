fff052e8c975e8c93f7dee1e0e04538da8be466f
Amanda da Silva <amandinhasilva1030@gmail.com>
2026-06-30 19:35:58 -0300

    volume certo
    fiz um volume a mais pra dentro do jogo e  coloquei o fundo no menu de pause

--- Geral/ScriptGeral/volume.gd
+++ Geral/ScriptGeral/volume.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_voltar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Geral/ScriptGeral/menuopcao.tscn")
+	get_tree().change_scene_to_file("res://Geral/ScriptGeral/meduopcao_conf.tscn")
