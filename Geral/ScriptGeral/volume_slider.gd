c0cad856d184c176721c80e5ad32eb63603ff13a
Amanda da Silva <amandinhasilva1030@gmail.com>
2026-06-30 20:22:02 -0300

    volume funcionando
    como o nome ja diz tá funcionando

--- Geral/ScriptGeral/volume_slider.gd
+++ Geral/ScriptGeral/volume_slider.gd
@@ -1,20 +1,24 @@
 extends HSlider
 
-@export
-var bus_name: String
+@export var bus_name: String = "Master"
 
 var bus_index: int
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
-	value_changed.connect(_on_value_changed)
-	
-	value = db_to_linear(
-		AudioServer.get_bus_volume_db(bus_index)
-	)
 
-func _on_value_changed(value: float) -> void:
+	if bus_index == -1:
+		push_error("Barramento '%s' não encontrado!" % bus_name)
+		return
+
+	if !value_changed.is_connected(_on_value_changed):
+		value_changed.connect(_on_value_changed)
+
+	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+
+
+func _on_value_changed(new_value: float) -> void:
 	AudioServer.set_bus_volume_db(
 		bus_index,
-		linear_to_db(value)
+		linear_to_db(new_value)
 	)
